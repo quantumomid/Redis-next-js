4a4307c04125380a3338b0b382029d6c8e3dc89a
Omid Wakili <omidwakili@Omids-MBP.home>
2024-02-05 19:00:48 +0000

    final commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,38 @@
-import Link from 'next/link'
+import { client } from "@/lib/db";
+import Link from "next/link";
 
 const getBooks = async () => {
-  
-}
+  const result = await client.zRangeByScoreWithScores("books", 0, -1);
+
+  const books = await Promise.all(
+    result.map(async (book) => {
+      const { score } = book;
+      return await client.hGetAll(`book:${score}`);
+    })
+  );
+  return books;
+};
 
 export default async function Home() {
-
+  const books = await getBooks();
   return (
     <main>
       <nav className="flex justify-between">
-        <h1 className='font-bold'>Books on Redis!</h1>
-        <Link href="/create" className="btn">Add a new book</Link>
+        <h1 className="font-bold">Books on Redis!</h1>
+        <Link href="/create" className="btn">
+          Add a new book
+        </Link>
       </nav>
-      
-      <p>List of books here.</p>
+
+      {
+        books.map((book) => (
+          <article key={book.title} className="card">
+            <h2 className="font-bold">{book.title}</h2>
+            <p>By {book.author}</p>
+            <p>{book.year}</p>
+          </article>
+        ))
+      }
     </main>
-  )
+  );
 }
